e6efb5ef6be6983edb08c7835e98d4c79670300d
yonguelink <isaac.computing@gmail.com>
2023-01-29 15:55:19 -0500

    Enable running another program on top of the game

--- steam-game-launcher/Launcher.cs
+++ steam-game-launcher/Launcher.cs
@@ -6,14 +6,17 @@ namespace steam_game_launcher {
   internal static class Launcher {
     private static void Main(string[] args) {
       try {
-        if (args.Length != 3) {
+        if (args.Length < 3) {
           throw new ArgumentException(
-            $"Expected 3 arguments, got {args.Length}.\n" +
-            "Run like so: steam-game-launcher.exe <steamInstallPath> <steamGameId>\n" +
+            $"Expected at least 3 arguments, got {args.Length}.\n" +
+            "Run like so: steam-game-launcher.exe <steamInstallPath> <steamGameId> <gameName> <asAdmin> <extraProgramPath> <extraProgramName>\n" +
             "Where: \n" +
             "\t<steamInstallPath> is the Path to Steam installation Folder (this folder needs to contain the `userdata` folder)\n" +
             "\t<steamGameId> is the ID steam gave to the game (also works with non-steam game as steam assign the shortcut an ID)\n" +
             "\t<gameName> is (part of) the game's name of the game\n" +
+            "\t<asAdmin> is either \"true\" or \"false\" to determine if everything should be run as admin (this is optional) - default is true\n" +
+            "\t<extraProgramPath> is the full path to a program you want to also start at the same time (this is optional) - default is none\n" +
+            "\t<extraProgramName> is the name of the extra program you want ot start (this is optional, but required if the extra program path is set)\n" +
             "NOTE: All Paths can be written with forward slash instead of double-backslash for simplicity"
           );
         }
@@ -21,13 +24,35 @@ namespace steam_game_launcher {
         string steamInstallPath = args[0].Replace('/', '\\');
         string steamGameId = args[1];
         string gameName = args[2];
+        string asAdmin = "true";
+        string extraProgramPath = null;
+        string extraProgramName = null;
+
+        if (args.Length >= 4) {
+          asAdmin = args[3];
+
+          if (args.Length == 6) {
+            extraProgramPath = args[4];
+            extraProgramName = args[5];
+          }
+        }
+
+        bool runAsAdmin = asAdmin == "true" ? true : false;
 
         KillProcesses(gameName.ToLower());
         KillProcesses("steam");
-        StartGame(steamInstallPath, steamGameId);
+        StartGame(steamInstallPath, steamGameId, runAsAdmin);
+
         KillProcesses(gameName.ToLower());
         KillProcesses("steam");
-        StartGame(steamInstallPath, steamGameId);
+        StartGame(steamInstallPath, steamGameId, runAsAdmin);
+
+        if (extraProgramName == null) {
+          return;
+        }
+        
+        KillProcesses(extraProgramName.ToLower());
+        StartProgram(extraProgramPath, runAsAdmin);
 
       } catch (Exception err) {
         Console.Write(err);
@@ -46,27 +71,43 @@ namespace steam_game_launcher {
       });
     }
 
-    private static void StartGame(string steamInstallPath, string steamGameId) {
+    private static void StartGame(string steamInstallPath, string steamGameId, bool asAdmin) {
       // Trick to enable starting another process than steam w/ steamGameId being the whole path to EXE
       if (steamInstallPath.ToLower() == "none") {
-        var nonSteamGameStartInfo = new ProcessStartInfo {
-          FileName = steamGameId
-        };
-        Process.Start(nonSteamGameStartInfo);
+        StartProgram(steamGameId, asAdmin);
         return;
       }
 
       var steamStartInfo = new ProcessStartInfo {
         FileName = $"{steamInstallPath}/steam.exe",
-        Verb = "runas",
         Arguments = "-silent"
       };
+
+      if (asAdmin) {
+        steamStartInfo.Verb = "runas";
+      }
+
       Process.Start(steamStartInfo);
 
       var gameStartInfo = new ProcessStartInfo {
         FileName = $"steam://rungameid/{steamGameId}"
       };
       Process.Start(gameStartInfo);
+    }
+
+    private static void StartProgram(string programPath, bool asAdmin) {
+      if (programPath == null) {
+        return;
+      }
+
+      var programStartInfo = new ProcessStartInfo {
+          FileName = programPath
+      };
+      if (asAdmin) {
+        programStartInfo.Verb = "runas";
+      }
+
+      Process.Start(programStartInfo);
     }
   }
 }
